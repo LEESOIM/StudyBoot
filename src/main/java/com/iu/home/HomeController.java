b6cd4679ce77101032008afde6f871fa0cdc2479
LEESOIM <>
2022-10-21 12:12:53 +0900

    beapo

--- src/main/java/com/iu/home/HomeController.java
+++ src/main/java/com/iu/home/HomeController.java
@@ -21,6 +21,10 @@ public class HomeController {
 	@Value("${my.message.hi}") //properties에서 가지고 온 값을 멤버변수에 주입 
 	private String message;
 	
+	@Value("${my.default}") 
+	private String app;
+	
+	
 	private final Logger log = LoggerFactory.getLogger(HomeController.class); //이 클래스 내에 로그기록을 찍을거야
 	//private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
@@ -29,11 +33,17 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String home() throws Exception {
-		log.error("Error Message");
-		log.warn("Warn Message");
-		log.info("Info Message");
-		log.debug("Debug Message");
-		log.trace("Trace Message");
+		log.error("--------------------");
+		log.error("message {} ", message);
+		log.error("default {} ", app);
+		log.error("--------------------");
+		
+		
+//		log.error("Error Message");
+//		log.warn("Warn Message");
+//		log.info("Info Message");
+//		log.debug("Debug Message");
+//		log.trace("Trace Message");
 		
 //		List<QnaVO> ar = qnaMapper.getList();
 //		log.info("List : {} size : {}", ar, ar.size()); //중괄호 안에 ar을 집어넣어라
